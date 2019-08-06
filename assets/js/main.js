c9bb43e6c62e5e32b2d74d5dfb47adc50ff61351
mark_calderon <mark_calderon@commude.ph>
2019-08-06 17:41:37 +0800

    create a todoapp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-Vue.component('tasklist', {
+Vue.component('tasklist-current', {
   	template: `
-		<li>{{ title }} 
+		<li class="list__item">
+		<span class="list__text">{{ title }}</span> 
 		<button class="btn__delete" v-on:click="$emit('remove')">Remove</button>
+		<button class="btn__done" v-on:click="$emit('finish')">Done</button>
+		</li>
+  	`,
+
+  	// Props should be v-bind on the v-for loop
+  	props: ['title'],
+});
+
+Vue.component('tasklist-complete', {
+  	template: `
+		<li class="list__item">
+		<span class="list__text">{{ title }}</span> 
 		</li>
   	`,
 
@@ -15,33 +28,51 @@ var app = new Vue({
 		nextTodoID: 1,
 		newTodoText: '',
 		todos: [],
+		todosComplete: [],
 		currentTask: true,
 		pastTask: false,
 		completeTask: false,
 	},
 	methods: {
 		addNewTask: function () {
-			this.todos.push({
-				id: this.nextTodoID++,
-				title: this.newTodoText,
-				status: 'current'
-			});
-			this.newTodoText = '';
+			if(!this.newTodoText == '') {
+
+				this.todos.push({
+					id: this.nextTodoID++,
+					title: this.newTodoText,
+					status: 'current'
+				});
+				this.newTodoText = '';
+			}
 		},
 		openTab: function(tab) {
 			if(tab == 1) {
-				this.pastTask = false;
 				this.completeTask = false;
 				this.currentTask = true;
-			}else if(tab == 2) {
+			}
+			else {
 				this.currentTask = false;
-				this.completeTask = false;
-				this.pastTask = true;
-			}else if(tab== 3){
-				this.currentTask = false;
-				this.pastTask = false;
 				this.completeTask = true;
 			}
+		},
+		taskFinish: function(idReceived) {
+			for(var key in this.todos){
+				if(key == idReceived) {
+					this.todos[key].status = 'done';
+
+					this.todosComplete.push({
+						id: this.nextTodoID++,
+						title: this.todos[key].title,
+						status: 'complete'
+					});
+
+					this.todos.splice(key, 1);
+				}
+			}
+			console.table(this.todosComplete);
+		},
+
+		startTimer: function() {
 		}
 	},
 });
